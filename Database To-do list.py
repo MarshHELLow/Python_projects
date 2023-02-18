445f3765a10ff6ffe9d387033207f4f03de92a63
Vladislav Chetvergov <117739126+MarshHELLow@users.noreply.github.com>
2023-02-18 21:50:59 +0300

    Add files via upload

--- Database To-do list.py
+++ Database To-do list.py
@@ -8,7 +8,6 @@ Base = declarative_base()
 
 class Task(Base):
     __tablename__ = 'task'
-
     id = Column(Integer, primary_key=True)
     task = Column(String, default='default_value')
     deadline = Column(Date, default=datetime.today())
@@ -21,32 +20,36 @@ engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-count_row = 0
 
 
-def main():
-    global first_row
+def menu():
     print("1) Today's tasks\n2) Add a task\n0) Exit")
     input_user = int(input())
-    if input_user == 0:
-        print("Bye!")
-        exit()
+    if input_user == 1:
+        today_task()
     elif input_user == 2:
         print("Enter a task")
-        add_task = input()
-        new_row = Task(task=add_task)
-        session.add(new_row)
-        session.commit()
-        rows = session.query(Task).all()
-        first_row = rows[0]
-        main()
-    elif input_user == 1:
-        print("Today:")
-        if count_row == 0:
-            print("Nothing to do!")
-        else:
-            print(f"{first_row.id}. {first_row.task}")
-        main()
+        add_task()
+    elif input_user == 0:
+        print("\nBye!")
+        exit()
 
 
-main()
+def today_task():
+    rows = session.query(Task).all()
+    if len(rows) == 0:
+        print("\nToday:\nNothing to do!")
+    else:
+        for row in rows:
+            print(f"{row.id}. {row.task}")
+    menu()
+
+
+def add_task():
+    new_row = Task(task=input())
+    session.add(new_row)
+    session.commit()
+    menu()
+
+
+menu()
