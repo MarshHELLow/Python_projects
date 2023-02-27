e114e493308d119d9fa691cde410efce5fc966df
Vladislav Chetvergov <117739126+MarshHELLow@users.noreply.github.com>
2023-02-27 22:00:46 +0300

    Add files via upload

--- Database To-do list.py
+++ Database To-do list.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from datetime import datetime, timedelta
 
 Base = declarative_base()
 
@@ -20,15 +20,19 @@ engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
+today = datetime.today()
 
 
 def menu():
-    print("1) Today's tasks\n2) Add a task\n0) Exit")
+    print("\n1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Add a task\n0) Exit")
     input_user = int(input())
     if input_user == 1:
         today_task()
     elif input_user == 2:
-        print("Enter a task")
+        week_task()
+    elif input_user == 3:
+        all_task()
+    elif input_user == 4:
         add_task()
     elif input_user == 0:
         print("\nBye!")
@@ -38,17 +42,52 @@ def menu():
 def today_task():
     rows = session.query(Task).all()
     if len(rows) == 0:
-        print("\nToday:\nNothing to do!")
+        print(f"Today {today.day} {today.strftime('%b')}:\nNothing to do!")
     else:
         for row in rows:
-            print(f"{row.id}. {row.task}")
+            if row.deadline == today.date():
+                print(f"{row.id}. {row.task}")
+    menu()
+
+
+def week_task():
+    end_of_week = today + timedelta(days=7)
+    tasks = session.query(Task).filter(Task.deadline >= today.date(), Task.deadline <= end_of_week.date()).order_by(Task.deadline).all()
+    for i in range(7):
+        current_day = today + timedelta(days=i)
+        tasks_for_current_day = [task for task in tasks if task.deadline == current_day.date()]
+        print(f"{current_day.strftime('%A %d %b')}:")
+        if len(tasks_for_current_day) == 0:
+            print("Nothing to do!\n")
+        else:
+            for j, task in enumerate(tasks_for_current_day):
+                print(f"{j+1}. {task.task}")
+            print()
+    menu()
+
+
+def all_task():
+    tasks = session.query(Task).order_by(Task.deadline).all()
+    if len(tasks) == 0:
+        print("Nothing to do!")
+    else:
+        print("\nAll tasks:")
+        for i, task in enumerate(tasks):
+            deadline_all_task = task.deadline.strftime("%#d %b")
+            print(f"{i+1}. {task.task}. {deadline_all_task}")
     menu()
 
 
 def add_task():
-    new_row = Task(task=input())
+    print("\nEnter a task:")
+    task_input = input()
+    print("\nEnter a deadline:")
+    deadline_input = input()
+    deadline = datetime.strptime(deadline_input, "%Y-%m-%d").date()
+    new_row = Task(task=task_input, deadline=deadline)
     session.add(new_row)
     session.commit()
+    print("\nThe task has been added!")
     menu()
 
 
